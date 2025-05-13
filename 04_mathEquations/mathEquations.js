734e802a48e5655a885ed7b40884afb53efa495a
synnestorm <synne.storm@gmail.com>
2025-05-13 15:30:24 +0200

    c is 66

--- 04_mathEquations/mathEquations.js
+++ 04_mathEquations/mathEquations.js
@@ -1,6 +1,6 @@
 const a = 1 - 1; // Freebie!!! This is the answer to "one minus one"
 const b = 1 + 8; // "one plus eight"
-const c = "22 times three";
+const c = 22 * 3; //"22 times three";
 const d = "the *remainder* of 5/4";
 const e = "the variable 'b' minus 17";
 const f = "the sum of the previous five variables";
